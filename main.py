80f1be4eaa8c4f8f30af6768bae478b5fea2b8ff
Ayush Bikram <ghimireayush52@gmail.com>
2024-02-20 13:17:08 +0545

    another

--- main.py
+++ main.py
@@ -6,3 +6,36 @@ from PIL import Image
 from tkinter import messagebox
 from io import BytesIO
 import  os
+class Stegno:
+
+    art ='''¯\_(ツ)_/¯'''
+    art2 = '''
+||'''
+    output_image_size = 0
+
+    def main(self,root):
+        root.title('ImageSteganography')
+        root.geometry('800x600')
+        root.resizable(width =False, height=False)
+        f = Frame(root)
+
+        title = Label(f,text='Image Steganography')
+        title.config(font=('courier',33))
+        title.grid(pady=10)
+
+        b_encode = Button(f,text="Encode",command= lambda :self.frame1_encode(f), padx=14)
+        b_encode.config(font=('courier',14))
+        b_decode = Button(f, text="Decode",padx=14,command=lambda :self.frame1_decode(f))
+        b_decode.config(font=('courier',14))
+        b_decode.grid(pady = 12)
+
+        ascii_art = Label(f,text=self.art)
+        # ascii_art.config(font=('MingLiU-ExtB',50))
+        ascii_art.config(font=('courier',60))
+
+        ascii_art2 = Label(f,text=self.art2)
+        # ascii_art.config(font=('MingLiU-ExtB',50))
+        ascii_art2.config(font=('courier',12,'bold'))
+
+        root.grid_rowconfigure(1, weight=1)
+        root.grid_columnconfigure(0, weight=1)
