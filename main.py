2343dbef22b52e5c479883cea07ce559a8d285e1
Ayush Bikram <ghimireayush52@gmail.com>
2024-02-20 13:14:26 +0545

    ok

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import tkinter as tk
-from tkinter import ttk, scrolledtext, messagebox, filedialog, Menu
-import socket
-import threading
-import ipaddress
-
-
-# Function to validate an IP address
-def valid_ip(address):
-    try:
-        ipaddress.ip_address(address)
-        return True
-    except ValueError:
-        return False
+from tkinter import *
+from tkinter import ttk
+import tkinter.filedialog
+from PIL import ImageTk
+from PIL import Image
+from tkinter import messagebox
+from io import BytesIO
+import  os
